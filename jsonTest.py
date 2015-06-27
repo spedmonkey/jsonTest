f532c179f5528154a6555612843a7ea27ce116eb
spedmonkey <spedcr@gmail.com>
2015-06-27 22:53:50 +1200

    First commit adding list of dictionaries to .json file

---
+++ jsonTest.py
@@ -0,0 +1,25 @@
+__author__ = 'spedmonkey'
+
+import json
+import datetime
+myList=[]
+myData = {'This':13, "awww":14, 'time':'24:00'}
+
+#Read data from .json file
+try:
+    with open('/home/spedmonkey/PycharmProjects/jsonTest/data') as data_file:
+        asdf = json.load(data_file)
+except:
+    pass
+
+#Create list to write
+try:
+    myList=asdf
+    myList.append(myData)
+except:
+    myList = [myData]
+
+#write data to .json file,,in this case a list of dictionaries.
+with open('/home/spedmonkey/PycharmProjects/jsonTest/data', mode='w') as feedsjson:
+    json.dump(myList, feedsjson, indent=4)
+    feedsjson.write('\n')
